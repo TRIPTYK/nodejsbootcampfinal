f697cd32b35870b7eb6cef019c26aceb8f75b62e
wendyfreedom31 <wendyfreedom31@hotmail.com>
2015-12-08 10:12:44 +0100

    url-manager split URL path

--- back/middlewares/url-manager.js
+++ back/middlewares/url-manager.js
@@ -7,20 +7,25 @@ let express = require('express'),
 function urlManager() {
   return function(req, res, next) {
     let pages = require(path.join(__dirname, "../models/pages"));
-    let temp = req.path.substring(1);
-    let pathBefore, pathAfter;
-    console.log(temp);
+    let completePath = req.path;
 
-    if(temp.indexOf("/") != -1){
-      pathBefore = "/" + temp.substring(0, temp.indexOf("/"));
-      pathAfter = temp.substring(temp.indexOf("/"));
-      console.log(pathBefore + "     " + pathAfter);
-    }else{
-      pathBefore = "/" + temp;
+    let pathBefore = completePath;
+  //  console.log(pathBefore);
+    let page;
+    let urlValid = false;
+
+    page = pages.getPageByUrl(completePath);
+    if(page != undefined) urlValid = true;
+
+    while(urlValid == false){
+        pathBefore = path.dirname(pathBefore);
+        page = pages.getPageByUrl(pathBefore);
+        if(page != undefined) urlValid = true;
     }
-    let page = pages.getPageByUrl(pathBefore);
-    res.render(page.template,page);
-    // next();
+
+    if(  pathBefore != "/" || completePath == "/" ) res.render(page.template,page);
+    else next();
+
   }
 }
 
