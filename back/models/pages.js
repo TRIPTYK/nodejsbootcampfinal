5f9d160396366373d6b6c8d7f1b5cc25fe50aaae
TheDigitalCrazyPainter <gilles@triptyk.eu>
2015-12-08 12:03:24 +0100

    V2 testing

--- back/models/pages.js
+++ back/models/pages.js
@@ -1,13 +1,14 @@
-"use strict";
-let path = require('path'),
-  fs = require('fs'),
-  jsonFile = path.join(__dirname, '../datas/pages.json'),
-  _ = require('lodash');
+'use strict';
+let path = require('path');
+let fs = require('fs');
+let jsonFile = path.join(__dirname, '../datas/pages.json');
+let _ = require('lodash');
 
 function Pages(next) {
   let pages = null;
   let dataIsLoaded = false;
 
+  //PRIVATE
   function initModel(jsonURL) {
     fs.readFile(jsonURL, function(err, data) {
       if (err) throw err;
@@ -16,19 +17,26 @@ function Pages(next) {
     });
   }
 
-  function getPages() {
-    if (dataIsLoaded) {
-      return pages;
-    } else {
-      throw "Data is not loaded yet";
-    }
+  function getPossibleUrl(urlStr) {
+    console.log("getPossibleUrl");
+    let tempArr = urlStr.split("/");
+    return tempArr;
   }
+
+  function findURLByUrl(urlStr) {
+    console.log(getPossibleUrl(urlStr));
+    let pageObj = _.find(pages, {
+      "url": url
+    });
+    return pageObj;
+  }
+
+
+  //PUBLIC
 
   function getPageByUrl(url) {
     if (dataIsLoaded) {
-      let pageObj = _.find(pages,{
-        "url": url
-      });
+      findURLByUrl(url);
       return pageObj;
     } else {
       throw "Data is not loaded yet";
@@ -36,8 +44,8 @@ function Pages(next) {
   }
 
   let that = {};
-  that.getPages = getPages;
   that.getPageByUrl = getPageByUrl;
+
   initModel(jsonFile);
   return that;
 }
