1aa1dc9c20de0cd4b98c95dcd9c7db72cc4e8ef6
wendyfreedom31 <wendyfreedom31@hotmail.com>
2015-12-08 08:36:39 +0100

    change npm start settings to use nodemon

--- back/middlewares/url-manager.js
+++ back/middlewares/url-manager.js
@@ -7,7 +7,18 @@ let express = require('express'),
 function urlManager() {
   return function(req, res, next) {
     let pages = require(path.join(__dirname, "../models/pages"));
-    let page = pages.getPageByUrl(req.path);
+    let temp = req.path.substring(1);
+    let pathBefore, pathAfter;
+    console.log(temp);
+
+    if(temp.indexOf("/") != -1){
+      pathBefore = "/" + temp.substring(0, temp.indexOf("/"));
+      pathAfter = temp.substring(temp.indexOf("/"));
+      console.log(pathBefore + "     " + pathAfter);
+    }else{
+      pathBefore = "/" + temp;
+    }
+    let page = pages.getPageByUrl(pathBefore);
     res.render(page.template,page);
     // next();
   }
