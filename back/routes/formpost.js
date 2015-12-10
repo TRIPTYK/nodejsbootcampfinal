47454bebf7e762d16b6f69f61227d6f18574e1b2
wendyfreedom31 <wendyfreedom31@hotmail.com>
2015-12-10 13:56:26 +0100

    send mail

--- back/routes/formpost.js
+++ back/routes/formpost.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
 
 /*POST FORM*/
 router.post('/formpost', function(req, res, next) {
-  console.log("eeee");
-  res.render('formpost');
+  console.log(req.body.name);
+  var transporter = nodemailer.createTransport();
+  transporter.sendMail({
+      from: req.body.email,
+      to: 'wendyfreedom31@hotmail.com',
+      subject: req.body.name + " " + req.body.firstName,
+      text: req.body.message
+  });
+//  res.render('formpost');
 
 });
 
