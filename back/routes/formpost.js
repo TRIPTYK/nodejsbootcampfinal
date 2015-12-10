d9aa00b3a730ed87cbf165eca7c9fa5c31efbda9
TheDigitalCrazyPainter <gilles@triptyk.eu>
2015-12-10 14:15:09 +0100

    add mailer

--- back/routes/formpost.js
+++ back/routes/formpost.js
@@ -2,18 +2,33 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
 
 /*POST FORM*/
 router.post('/formpost', function(req, res, next) {
-  console.log(req.body.name);
-  var transporter = nodemailer.createTransport();
+  var options = {
+    service: 'gmail',
+    auth: {
+      user: "",
+      pass: ""
+    }
+
+  }
+  var transporter = nodemailer.createTransport(smtpTransport(options));
   transporter.sendMail({
-      from: req.body.email,
-      to: 'wendyfreedom31@hotmail.com',
-      subject: req.body.name + " " + req.body.firstName,
-      text: req.body.message
+    from: req.body.email,
+    to: 'gilles@triptyk.eu',
+    subject: req.body.name + " " + req.body.firstName,
+    text: req.body.message
+  }, function(error, response) {
+
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Message sent: " + response.message);
+    }
   });
-//  res.render('formpost');
+  //  res.render('formpost');
 
 });
 
